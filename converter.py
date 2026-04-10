aaa99d336a69ca7a099ee1c5a656e57c65a904cc
Goldenn Vscodov <Sraniparol1@gmail.com>
2026-04-11 01:26:57 +0500

    Результат конвертации переведен в формат JSON. Исправление #3

--- converter.py
+++ converter.py
@@ -1,19 +1,37 @@
+import json
+
 def main():
-    # Курсы валют для задачи №2
+    # Курсы валют для задачи №2 и №3
     rates = {"USD": 92.5, "EUR": 100.2}
-
-    print("--- Конвертер Валют v1.1 ---")
+    
+    print("--- Микросервис Конвертации v1.2 (JSON) ---")
     currency = input("Выберите валюту (USD или EUR): ").upper()
-
+    
     if currency in rates:
         try:
-            amount = float(input(f"Введите сумму в {currency}: "))
-            result = amount * rates[currency]
-            print(f"Итог: {amount} {currency} = {result:.2f} RUB")
+            amount_str = input(f"Введите сумму в {currency}: ")
+            amount = float(amount_str)
+            result_rub = amount * rates[currency]
+            
+            # Формируем JSON (Требование Issue #3)
+            response = {
+                "status": "success",
+                "data": {
+                    "from": currency,
+                    "to": "RUB",
+                    "amount": amount,
+                    "result": round(result_rub, 2)
+                }
+            }
+            print(json.dumps(response, indent=4, ensure_ascii=False))
+            
         except ValueError:
-            print("Ошибка: введите число!")
+            error_msg = {"status": "error", "message": "Нужно ввести число"}
+            print(json.dumps(error_msg, indent=4, ensure_ascii=False))
     else:
-        print("Ошибка: Данная валюта не поддерживается.")
+        error_msg = {"status": "error", "message": "Валюта не поддерживается"}
+        print(json.dumps(error_msg, indent=4, ensure_ascii=False))
 
-if name == "main":
+
+if __name__ == "__main__":
     main()
